856299ba62ef08c01538038b56486437cdfbc40f
hhuang42 <hhuang@g.hmc.edu>
2014-11-15 17:44:55 -0800

    Added usbmsd to interface with a USB MSD
    MPLAB project that writes text to a file that works with the microMudd
    board.

---
+++ usbmsd/Objects/C32/usb_host_msd.d
@@ -0,0 +1,5 @@
+Objects\C32\usb_host_msd.o: USB\MSD\ Host\ Driver\usb_host_msd.c \
+ Include/Compiler.h HardwareProfile.h Include/uart2.h Include/USB\usb.h \
+ usb_config.h Include/usb/usb_common.h Include/usb/usb_ch9.h \
+ Include/usb/usb_host.h Include/usb/usb_hal.h Include/USB\usb_hal_pic32.h \
+ Include/USB\usb_host_msd.h
