3d8fd74df190541248cdb482c31eb4d17197745d
hhuang42 <hhuang@g.hmc.edu>
2014-11-17 02:17:32 -0800

    Revert "revert this commit"
    This reverts commit dbf5af75e1b2219162dc41d5153c2126977230ca.

--- usbmouse/Objects/C32/usb_host.d
+++
@@ -1,6 +0,0 @@
-Objects\C32\usb_host.o: USB\usb_host.c Include/GenericTypeDefs.h \
- Include/USB\usb.h Include/Compiler.h usb_config.h \
- Include/USB/usb_common.h Include/USB/usb_ch9.h Include/USB/usb_host.h \
- Include/USB/usb_hal.h Include/USB/usb_hal_pic32.h USB\/usb_host_local.h \
- USB\/usb_hal_local.h Include/usb/usb.h USB\/usb_pic32.h \
- USB\/usb_hal_local.h HardwareProfile.h Include/uart2.h
--- usbmouse/Objects/C32/usb_host_msd.d
+++
@@ -1,5 +0,0 @@
-Objects\C32\usb_host_msd.o: USB\MSD\ Host\ Driver\usb_host_msd.c \
- Include/Compiler.h HardwareProfile.h Include/uart2.h Include/USB/usb.h \
- usb_config.h Include/USB/usb_common.h Include/USB/usb_ch9.h \
- Include/USB/usb_host.h Include/USB/usb_hal.h Include/USB/usb_hal_pic32.h \
- Include/USB/usb_host_msd.h
